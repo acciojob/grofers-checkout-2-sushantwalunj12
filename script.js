2bf4228ec675506adfc6e139639bb35969f5d7e1
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-03-22 20:21:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const prices = document.querySelectorAll('[data-ns-test="prices"]');
-let total = 0;
-
-prices.forEach((price) => {
-  const priceValue = parseFloat(price.innerHTML);
-  total += priceValue;
+const getTotalBtn = document.getElementById("getTotalBtn");
+getTotalBtn.addEventListener("click", () => {
+  const prices = document.querySelectorAll('[data-ns-test="prices"]');
+  let total = 0;
+  for (let i = 0; i < prices.length; i++) {
+    total += parseFloat(prices[i].textContent);
+  }
+  const totalRow = document.createElement("tr");
+  totalRow.innerHTML = `
+    <td></td>
+    <td data-ns-test="grandTotal">${total}</td>
+  `;
+  document.getElementById("table").appendChild(totalRow);
 });
-
-const table = document.querySelector('table');
-const newRow = table.insertRow();
-const newCell = newRow.insertCell();
-newCell.innerHTML = total;
-newCell.setAttribute('data-ns-test', 'grandTotal');
