d7c6914fbb26c0597b91df891707d68ea41fe800
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-03-22 10:40:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your code here
+const prices = document.querySelectorAll('[data-ns-test="prices"]');
+let total = 0;
+
+prices.forEach((price) => {
+  const priceValue = parseFloat(price.innerHTML);
+  total += priceValue;
+});
+
+const table = document.querySelector('table');
+const newRow = table.insertRow();
+const newCell = newRow.insertCell();
+newCell.innerHTML = total;
+newCell.setAttribute('data-ns-test', 'grandTotal');
