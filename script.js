00ddfe4379237286dc9ad4e50fad34f69a457af4
Sushant Walunj <88195944+sushantwalunj12@users.noreply.github.com>
2023-03-22 20:37:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-const getTotalBtn = document.getElementById("getTotalBtn");
-getTotalBtn.addEventListener("click", () => {
-  const prices = document.querySelectorAll('[data-ns-test="prices"]');
-  let total = 0;
-  for (let i = 0; i < prices.length; i++) {
-    total += parseFloat(prices[i].textContent);
-  }
-  const totalRow = document.createElement("tr");
-  totalRow.innerHTML = `
-    <td></td>
-    <td data-ns-test="grandTotal">${total}</td>
-  `;
-  document.getElementById("table").appendChild(totalRow);
-});
+const getSumBtn = document.createElement("button");
+getSumBtn.append("Total Price");
+document.body.appendChild(getSumBtn);
+
+const getSum = () => {
+//Add your code here
+	let prices = document.querySelectorAll('.price');
+	let sum = 0;
+	prices.forEach(price => {
+		sum += parseFloat(price.textContent);
+	});
+	document.getElementById('table').innerHTML += `
+	   <tr id="ans">
+          <td></td>
+          <td>${sum}</td>
+        </tr>`
+};
+
+getSumBtn.addEventListener("click", getSum)
